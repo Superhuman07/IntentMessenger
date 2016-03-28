b24bb7c4eb77d4a068761b0a02ab7d0a27cefe81
Superhuman07 <Superhuman07>
2016-03-28 23:22:02 +0530

    Add intent message within the app

--- app/src/main/java/com/example/admin/intentmessenger/MainActivity.java
+++ app/src/main/java/com/example/admin/intentmessenger/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.admin.intentmessenger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSendMessage(View view)
+    {
+        Intent intent = new Intent(this, Main2Activity.class);
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String value = messageView.getText().toString();
+        intent.putExtra("message", value);
+        startActivity(intent);
+
+
     }
 }
